cec3008a2c6ff1d6adb431324c9250e1a782b769
Nathaniel Weeks <nweeks87@gmail.com>
2017-11-02 13:10:10 -0400

    add student creation

--- app/javascript/react/src/containers/IndexPage.js
+++ app/javascript/react/src/containers/IndexPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import StudentTile from '../components/StudentTile'
+import NewStudentFormContainer from './NewStudentFormContainer'
 
 
 class IndexPage extends Component{
@@ -9,10 +10,11 @@ class IndexPage extends Component{
     this.state = {
       studentsArray: []
     }
+    this.addNewStudent = this.addNewStudent.bind(this)
   }
 
   componentDidMount() {
-    fetch(`/api/v1/classrooms/${this.props.match.params.id}/students.json`, {
+    fetch(`/api/v1/classrooms/${this.props.match.params.id}/students`, {
       credentials: 'same-origin',
       method: 'GET',
       headers: { 'Content-Type':'application/json'}
@@ -33,6 +35,23 @@ class IndexPage extends Component{
       .catch(error => console.error(`Error in fetch: ${error.message}`));
     }
 
+    addNewStudent(formPayload){
+      let id = this.props.match.params.id
+      fetch(`/api/v1/classrooms/${id}/students`,{
+        credentials: 'same-origin',
+        headers: {
+          'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({ student: formPayload })
+    })
+    .then(response => response.json())
+    .then(body => {
+      this.setState({ studentsArray: this.state.studentsArray.concat(body) })
+        })
+    }
+
   render(){
     let students = this.state.studentsArray.map(student => {
       return(
@@ -50,6 +69,10 @@ class IndexPage extends Component{
 
     return(
       <div>
+      <NewStudentFormContainer
+      addNewStudent={this.addNewStudent}
+      classroom_id={this.props.match.params.id}
+      />
       <h1>Students:</h1>
       <p><Link to='/schools/:id/classrooms/:id/students/new'>Add a Student</Link></p>
       {students}
--- app/javascript/react/src/containers/NewStudentFormContainer.js
+++ app/javascript/react/src/containers/NewStudentFormContainer.js
@@ -7,17 +7,118 @@ class NewStudentFormContainer extends Component {
       firstName: '',
       lastName: '',
       address: '',
-      age: 0,
-      contactNumber: 999999999,
+      age: '',
+      contactNumber: '',
       errors: []
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.clearForm = this.clearForm.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  handleChange(event){
+    let value = event.target.value
+    let name = event.target.name
+    this.setState({ [name]: value})
+  }
+
+  clearForm(){
+    this.setState({
+      firstName: '',
+      lastName: '',
+      address: '',
+      age: '',
+      contactNumber: '',
+      errors: []
+    })
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    if(this.state.firstName == '' || this.state.lastName == '' || this.state.age == ''){
+      let error_array = []
+      if (this.state.firstName == '' || this.state.lastName == ''){
+        error_array.push("please fill out a first name and last name.")
+      } else if (this.state.age == ''){
+        error_array.push("please include an age.")
+      }
+      this.setState({errors: error_array})
+    } else {
+      let formPayload = {
+        first_name: this.state.firstName,
+        last_name: this.state.lastName,
+        address: this.state.address,
+        age: this.state.age,
+        phone_number: this.state.contactNumber,
+        classroom_id: this.props.classroom_id
+      }
+      this.props.addNewStudent(formPayload);
+      this.clearForm();
+    }
+  }
+
   render(){
+    let errors = this.state.errors
+    let error_messages = []
+    if(errors.length > 0){
+      error_messages = errors.map( error => {
+        return(
+          <li>{error}</li>
+        )
+      })
+    }
 
     return(
-      <div>
-        <p>hello form</p>
-      </div>
+  <form className='student-form'>
+    <h3 className='student-form-header'>New Student</h3>
+    <ul>{errors}</ul>
+    <label>First Name
+      <textarea
+        name={"firstName"}
+        value={this.state.firstName}
+        onChange={this.handleChange}
+        type="text"
+      />
+    </label>
+
+    <label>Last Name
+      <textarea
+        name={"lastName"}
+        value={this.state.lastName}
+        onChange={this.handleChange}
+        type="text"
+      />
+    </label>
+
+    <label>Address
+      <textarea
+        name={"address"}
+        value={this.state.address}
+        onChange={this.handleChange}
+        type="text"
+      />
+    </label>
+
+    <label>Age
+      <textarea
+        name={"age"}
+        value={this.state.age}
+        onChange={this.handleChange}
+        type="text"
+      />
+    </label>
+
+    <label>Phone Number
+      <textarea
+        name={"contactNumber"}
+        value={this.state.contactNumber}
+        onChange={this.handleChange}
+        type="text"
+      />
+    </label>
+
+    <button className="submit-button" type="submit" value="Submit" onClick={this.handleSubmit}>Submit</button>
+  </form>
     )
   }
 }
